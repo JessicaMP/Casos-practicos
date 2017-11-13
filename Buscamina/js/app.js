c003e8fe8505ae64db73deaf3e01a1e7a2cc8e6a
JessicaMP <jessica95.mp@gmail.com>
2017-11-12 23:29:23 -0500

    Trabajo completo

--- Buscamina/js/app.js
+++ Buscamina/js/app.js
@@ -1,39 +1,75 @@
-var board = document.querySelector('board-js');
-//var allCells = ;
-var BOM = "*",
-    SHOW = "X";
+var tableBomIt = [
+				  ' ',
+				  '*',
+				  '1',
+				  ' ',
+				  '/n',
+				  ' ',
+				  ' ',
+				  '*',
+				  '2',
+				  '/n',
+				  '1',
+				  '*',
+				  ' ',
+				  ' ',
+				  '/n',
+				  ' ',
+				  '3',
+				  '*',
+				  ' '
+				 ];
+var containerBombIt = document.getElementById('container-bomb-it');
+var bombIt = document.getElementById('bomb-it');
 
-var matrixOrigin = [[1, 1, 1,""],
-                    [1, BOM, 1, ""],
-                    [1,1,2,1],
-                    ["", "",1, BOM]];
-//board.addEventListener('click', displayCell);
+var drawGameZone = function(array) {
+	for (var i = 0; i < tableBomIt.length; i++) {
+		var cell = document.createElement('div');
+		var enter = document.createElement('br');
 
-function displayCell(event) {
-  if(event.target.matches('td')) { /*Al aplicar el evento a la estructura de tabla que solo se muestre esto cuando el usuario le aya hecho click a el td*/
-    var value = getCell(event)
+		if (array[i] == '1' || array[i] == '2' || array[i] == '3') {
+			cell.className = 'number';
+			cell.innerText = array[i];
+			cell.onclick = showNumber;
+		} else if (array[i] == ' ') {
+			cell.className = 'emptySpace';
+			cell.onclick = changeColor;
+		} else if (array[i] == '*') {
+			cell.className = 'bomb';
+			cell.onclick = boom;
+		} else if(array[i] == '/n') {
+			bombIt.appendChild(enter);
+			continue;
+		}
 
-    event.target.textContent = value;
+		bombIt.appendChild(cell);
 
-    if(value === BOMB) {
-      showMatrix('red');
-    }
-  }
+	}
+	addBtnRestar();
 };
 
-function getCell(event) {
-  var row = parseInt(target.parentElement.dataset.row);
-  var column = parseInt(event.target.parentElement.dataset.column);
-
-  return matrixOrigin[row - 1][column - 1];
+var showNumber = function() {
+	this.className = 'number numberShow'
 };
 
-function showMatrix(event, color) {
-  for (var i = 0; i< matrixOrigin.length; i++) {
-    for (var j = 0; j<matrixOrigin[i].length; j++) {
-      if(matrixOrigin[i][j]) {
-        allCells[i*4 + j].style.backgroundColor = color;
-      }
-    }
-  }
+var changeColor = function() {
+	this.style.backgroundColor = "#000";
 };
+
+var boom = function() {
+	containerBombIt.removeChild(bombIt);
+	containerBombIt.innerHTML = "<img src='http://www.gifsanimados.org/data/media/1176/explosion-imagen-animada-0001.gif'>";
+	addBtnRestar();
+};
+
+var addBtnRestar = function () {
+	var btnRestar = document.createElement('button');
+	btnRestar.innerText = 'Restart!!';
+	btnRestar.addEventListener('click', reStart);
+	containerBombIt.appendChild(btnRestar);
+};
+
+var reStart = function () {
+	location.reload();
+}
+drawGameZone(tableBomIt);
---
+++ README.md
@@ -0,0 +1,19 @@
+# **Casos Practicos**
+En este trabajo se puede ver cada uno de los de los siguientes ejercicios:
+### Aerolínea
+![Sin titulo](assets/images/aerolinea.png)
+
+### Buscaminas
+![Sin titulo](assets/images/bom.png)
+
+### Menu Burger
+![Sin titulo](assets/images/burger.gif)
+
+### Tabs
+![Sin titulo](assets/images/tabs.gif)
+
+### Menu Drop Down
+![Sin titulo](assets/images/menu-drop.gif)
+
+### Mini Zoo
+![Sin titulo](assets/images/mini-zoo.gif)
