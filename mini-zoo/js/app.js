baf7fa343de0b65b0d8ab769e137f300baf181a0
JessicaMP <jessica95.mp@gmail.com>
2017-11-13 14:50:44 -0500

    agregando aerolinea

--- mini-zoo/js/app.js
+++ mini-zoo/js/app.js
@@ -1,7 +1,7 @@
 var select = document.getElementById('select');
+var filter = document.getElementsByClassName('filter');
 select.onchange = function() {
   if (select.value == 'Original') {
-    var filter = document.getElementsByClassName('filter');
     for (var i = 0; i < filter.length; i++) {
       filter[i].classList.remove('black-white');
       filter[i].classList.remove('negative-colors');
@@ -9,27 +9,21 @@ select.onchange = function() {
     }
   }
   else if (select.value == 'Blanco y negro') {
-    var filter = document.getElementsByClassName('filter');
     for (var i = 0; i < filter.length; i++) {
-      /*filterimage[i].classList.remove('original');*/
       filter[i].classList.remove('negative-colors');
       filter[i].classList.remove('sepia');
       filter[i].classList.add('black-white');
     }
   }
   else if (select.value == 'Sepia') {
-    var filter = document.getElementsByClassName('filter');
     for (var i = 0; i < filter.length; i++) {
-      /*filterimage[i].classList.remove('original');*/
       filter[i].classList.remove('negative-colors');
       filter[i].classList.add('sepia');
       filter[i].classList.remove('black-white');
     }
   }
   else if (select.value == 'Invertir Colores') {
-    var filter = document.getElementsByClassName('filter');
     for (var i = 0; i < filter.length; i++) {
-      /*filterimage[i].classList.remove('original');*/
       filter[i].classList.add('negative-colors');
       filter[i].classList.remove('sepia');
       filter[i].classList.remove('black-white');
